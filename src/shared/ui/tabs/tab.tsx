17898ae4341d9a4b8c63a7215f5a3f7d42711ece
cgh96 <tt878429@gmail.com>
2026-01-11 15:15:27 +0900

    feat: Tabs 공통 컴포넌트 UI 구현

---
+++ src/shared/ui/tabs/tab.tsx
@@ -0,0 +1,24 @@
+import type { Option } from "@/shared/lib/types";
+
+export type TabProps = {
+  option: Option;
+  selected: boolean;
+  onClick: (tab: Option) => void;
+};
+
+export const Tab = ({ option, selected, onClick }: TabProps) => {
+  const selectedStyles = "bg-surface-alt text-accent-primary-foreground";
+  const unselectedStyles = "bg-surface-default text-text-muted";
+
+  const styles = selected ? selectedStyles : unselectedStyles;
+
+  return (
+    <button
+      type="button"
+      className={`flex-1 px-4 py-2 rounded-md text-base font-bold cursor-pointer hover:bg-surface-alt ${styles}`}
+      onClick={() => onClick(option)}
+    >
+      {option.label}
+    </button>
+  );
+};
---
+++ src/shared/ui/tabs/tabs.tsx
@@ -0,0 +1,24 @@
+import type { Option } from "@/shared/lib/types";
+
+import { Tab } from "./tab";
+
+export type TabSelectProps = {
+  tabs: Option[];
+  selectedTab: Option;
+  onChangeTab: (tab: Option) => void;
+};
+
+export const Tabs = ({ tabs, selectedTab, onChangeTab }: TabSelectProps) => {
+  return (
+    <div className="flex gap-1 p-1 bg-surface-default rounded-md">
+      {tabs.map((tab) => (
+        <Tab
+          key={tab.value}
+          option={tab}
+          selected={selectedTab.value === tab.value}
+          onClick={onChangeTab}
+        />
+      ))}
+    </div>
+  );
+};
