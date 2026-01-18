9b4bc409955b3b28bfb653f64079ca39f0b93ae6
cgh96 <tt878429@gmail.com>
2026-01-18 22:34:16 +0900

    feat: symbol-quote에 대해 ssr prfetch 적용

--- app/(main)/stock-dashboard/[symbol]/page.tsx
+++ app/(main)/stock-dashboard/[symbol]/page.tsx
@@ -1,5 +1,32 @@
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+
+import { QUOTE_QUERY_KEY } from "@/entities/quote/lib/use-get-quote-query";
+import { quoteService } from "@/server/service/quote.service";
 import { SymbolQuoteSection } from "@/widgets/symbol-quote/ui/symbol-quote-section";
 
-export default function StockDetailPage() {
-  return <SymbolQuoteSection />;
+const getQuote = async (symbol: string) => {
+  const result = await quoteService.getQuote(symbol);
+
+  if (result.ok) {
+    return result.data;
+  }
+
+  throw new Error(result.message);
+};
+
+export default async function StockDetailPage({ params }: { params: { symbol: string } }) {
+  const queryClient = new QueryClient();
+  const { symbol } = params;
+
+  await queryClient.prefetchQuery({
+    queryKey: [QUOTE_QUERY_KEY, symbol],
+    queryFn: () => getQuote(symbol),
+    staleTime: 1000 * 60 * 2,
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <SymbolQuoteSection />
+    </HydrationBoundary>
+  );
 }
