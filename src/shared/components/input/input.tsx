db8c2ff3693a147ab06392a70ea4cb1641d0ebbc
cgh96 <tt878429@gmail.com>
2026-03-22 19:37:18 +0900

    fix: iOS Safari 입력창 포커스 자동 확대 방지 (font-size 16px)

--- src/shared/components/input/input.tsx
+++ src/shared/components/input/input.tsx
@@ -17,8 +17,7 @@ export type InputProps = Omit<
 export function Input({ value, type, disabled, ...props }: InputProps) {
   return (
     <input
-      className="w-full border-border-default border-solid border px-2 py-1 rounded-md font outline-none focus:border-border-strong text-sm md:text-base
-      "
+      className="w-full border-border-default border-solid border px-2 py-1 rounded-md font outline-none focus:border-border-strong text-base"
       type={type}
       value={value}
       disabled={disabled}
